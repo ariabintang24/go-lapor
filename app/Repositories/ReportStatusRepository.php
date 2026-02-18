3a8d37f542e99c7b5233fe5f9bc4ef70302edf2c
aria bintang <ariabintang24@gmail.com>
2026-02-18 10:34:43 +0700

    add interface and repository for report status

---
+++ app/Repositories/ReportStatusRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ReportStatusRepositoryInterface;
+use App\Models\ReportStatus;
+
+class ReportStatusRepository implements ReportStatusRepositoryInterface
+{
+    public function getAllReportStatuses()
+    {
+        return ReportStatus::all();
+    }
+
+    public function getReportStatusById(int $id)
+    {
+        return ReportStatus::where('id', $id)->first();
+    }
+
+    public function createReportStatus(array $data)
+    {
+
+        return ReportStatus::create($data);
+    }
+
+    public function updateReportStatus(array $data, int $id)
+    {
+        $ReportStatus = $this->getReportStatusById($id);
+
+        return $ReportStatus->update($data);
+    }
+
+    public function deleteReportStatus(int $id)
+    {
+        $ReportStatusCategory = $this->getReportStatusById($id);
+
+        return $ReportStatusCategory->delete();
+    }
+}
